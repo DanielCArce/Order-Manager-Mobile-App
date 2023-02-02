97281050cc009c370805125fb06b0a77dbdd863d
DanielCArce <daniel.camposarce@gmail.com>
2023-02-02 10:44:50 -0600

    add: tab of ClientsProviders
    add a new tab of clients and ClientsProviders

--- src/navigations/TabNavigation.jsx
+++ src/navigations/TabNavigation.jsx
@@ -4,14 +4,16 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import DashboardTab from '../screens/Dashboard'
 import ReportsTab from '../screens/Reports'
 import SettingsTab from '../screens/Settings'
+import ClientsProviders from '../screens/ClientsProviders';
 
 const Tab = createBottomTabNavigator();
 function TabNavigation(){
   return (
-      <Tab.Navigator>
-      <Tab.Screen name="Dashboard" options={{headerShown:false}} component={DashboardTab} />
-      <Tab.Screen name='Reports' options={{headerShown:false}} component={ReportsTab} />
-      <Tab.Screen name='Settings' options={{headerShown:false}} component={SettingsTab} />
+      <Tab.Navigator screenOptions={{}}>
+      <Tab.Screen name="Dashboard" options={{headerShown:false, tabBarLabel:'Ordenes',}} component={DashboardTab} />
+      <Tab.Screen name='Reports' options={{ headerShown: false, tabBarLabel:'Reportes' }} component={ReportsTab} />
+      <Tab.Screen name='ClientsProviders' options={{ headerShown: false, tabBarLabel:'Clientes y Proveedores'}} component={ClientsProviders} />
+      <Tab.Screen name='Settings' options={{headerShown:false,tabBarLabel:'Configuración'}} component={SettingsTab} />
     </Tab.Navigator>
   )
 }
