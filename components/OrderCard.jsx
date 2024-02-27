35ee2e04c4bcae6c34d09c06c5ea3f93e3e4a6a4
DanielCArce <daniel.camposarce@gmail.com>
2024-02-27 09:55:19 -0600

    fix: add Key prop to components
    Adding props to handle errors

--- components/OrderCard.jsx
+++ components/OrderCard.jsx
@@ -1,9 +1,10 @@
 import { View, Text, Pressable } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
-
+import useOrder from '../hooks/useOrder';
 const OrderCard = ({ orderInfo }) => {
   const navigation = useNavigation()
+  const {addCurrentOrder } = useOrder()
   return (
     <View style={{paddingHorizontal:8, paddingVertical:8, borderColor:'#000000', borderWidth:1, marginHorizontal:8, marginVertical:10, justifyContent:'space-between', flexDirection:'row'} }>
       <View>
@@ -12,7 +13,10 @@ const OrderCard = ({ orderInfo }) => {
       <Text>Date: { Intl.DateTimeFormat('es-cr',{day:'2-digit', dayPeriod:'long', month:'2-digit', year:'2-digit', weekday:'long'}).format(orderInfo.date)}</Text>
       </View>
       <View>
-        <Pressable style={{backgroundColor:'#11ee32', paddingHorizontal:4, paddingVertical:8, borderRadius:6, marginBottom:5} } onPress={(event)=>{ navigation.navigate('OrderDetails',{orderID: orderInfo.orderID})}}>
+        <Pressable style={{ backgroundColor: '#11ee32', paddingHorizontal: 4, paddingVertical: 8, borderRadius: 6, marginBottom: 5 }} onPress={(event) => {
+          addCurrentOrder(orderInfo.orderID)
+          navigation.navigate('OrderDetails', { orderID: orderInfo.orderID })
+        }}>
           <Text style={{textAlign:'center'}}>Details</Text>
         </Pressable>
         <Pressable style={[{paddingHorizontal:4, paddingVertical:8, borderRadius:6},orderInfo.status == 'COMPLETED' ? {backgroundColor:'#008040'}: {backgroundColor:'#ff8040'}] }>
