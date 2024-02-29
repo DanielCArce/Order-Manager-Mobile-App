0b8c55a71ea6d1945f521a85c193990e9997ca16
DanielCArce <daniel.camposarce@gmail.com>
2024-02-29 16:15:19 -0600

    update config

--- App.js
+++ App.js
@@ -3,10 +3,9 @@ import MainNavigation from './navegations/MainNavigation'
 import AuthProvider from './contexts/AuthContext'
 import OrderProvider from './contexts/OrderContext';
 import * as Sentry from '@sentry/react-native';
-
 Sentry.init({
   dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
-  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  debug:true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
 });
 
 function App() {
@@ -20,4 +19,6 @@ function App() {
     </AuthProvider>
   );
 }
+
 export default Sentry.wrap(App)
+// export default App
