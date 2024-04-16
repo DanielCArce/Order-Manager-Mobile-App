781581be8d47398aa05010380530585a6f218618
DanielCArce <daniel.camposarce@gmail.com>
2024-04-15 23:56:14 -0600

    update structure app
    Removing structure of app

--- schemas/RequestNewPassword.js
+++
@@ -1,7 +0,0 @@
-import * as Yup from 'yup'
-
-const RequestNewPasswordScheme = Yup.object({
-    email: Yup.string().email().required('This field only accept emails')
-})
-
-export default RequestNewPasswordScheme
