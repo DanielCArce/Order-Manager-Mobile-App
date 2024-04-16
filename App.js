c85a080ccb252ddcd21d14369b985bc0ae2979c0
DanielCArce <daniel.camposarce@gmail.com>
2024-04-15 23:57:49 -0600

    Modified Providers
    Add AuthProvider and ContentProvider inside of
    App.js file while improve structure

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
 import { NavigationContainer} from '@react-navigation/native'
-import MainNavigation from './navegations/MainNavigation'
-import AuthProvider from './contexts/AuthContext'
-import OrderProvider from './contexts/OrderContext';
 import * as Sentry from '@sentry/react-native';
+import MainNav from './app/Navigations/MainNav';
+import AuthProvider from './app/Contexts/Auth';
+import ContentProvider from './app/Contexts/Content';
 Sentry.init({
   dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
   debug:true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
 });
 
 function App() {
-  return (
+  return (<>
     <AuthProvider>
-      <OrderProvider>
+      <ContentProvider>
         <NavigationContainer>
-          <MainNavigation/>
+            <MainNav/> 
         </NavigationContainer>
-      </OrderProvider>
+      </ContentProvider>
     </AuthProvider>
-  );
+  </>);
 }
 
 export default Sentry.wrap(App)
