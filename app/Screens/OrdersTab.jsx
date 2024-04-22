fc395d9621a9f4c83d49c01e07865de4b24f81ce
DanielCArce <daniel.camposarce@gmail.com>
2024-04-22 12:41:02 -0600

    add OrdersTab Screen

---
+++ app/Screens/OrdersTab.jsx
@@ -0,0 +1,14 @@
+import { View, Text, StatusBar } from 'react-native'
+import React from 'react'
+
+const OrdersTab = () => {
+    return (<>
+    <View>
+      <Text>OrdersTab</Text>
+      </View>
+      <StatusBar/>
+  </>
+  )
+}
+
+export default OrdersTab
