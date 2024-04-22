850b48b11e828ec7218b980c3d5a3ac7e098a18c
DanielCArce <daniel.camposarce@gmail.com>
2024-04-22 12:21:43 -0600

    add storage auth token
    add method token storage in disk
    with SecureStore

--- app/Hooks/useAuth.js
+++ app/Hooks/useAuth.js
@@ -3,40 +3,41 @@ import { AuthContext } from '../Contexts/Auth'
 import { AuthActions } from '../Reducers/Auth'
 import {getToken} from '../Services/Auth'
 import { useNavigation } from '@react-navigation/native'
-import {saveToken, removeToken} from '../Utils/Storage'
+import {recoveryTokenFromStorage, removeTokenFromStorage, saveTokenInStorage} from '../Utils/Storage'
 function useAuth() {
     const { state, dispatch } = useContext(AuthContext)
     const navigation = useNavigation()
     const SignIn = (credentials) => {
         getToken(credentials).then((token) => {
-            
             if (token != undefined || token != null) {
                 dispatch({ type: AuthActions.ADD_TOKEN, payload: token })
-                saveToken('token', token)
-                return navigation.navigate('HomeScreen')
+                saveTokenInStorage(token).then(function (v) {
+                    return navigation.navigate('HomeScreen')
+                })
             }
             return navigation.navigate('LoginScreen')
         })
     }
     const SignOut = () => {
         dispatch({ type: AuthActions.REMOVE_TOKEN })
-        removeToken()
-        return navigation.navigate('LoginScreen')
-
-    }
-    const setToken = (token) => {
-        
-        if (token == undefined) {
+        removeTokenFromStorage().then(function () {
             return navigation.navigate('LoginScreen')
-        }
-        dispatch({ type: AuthActions.ADD_TOKEN, payload: token })
-        return navigation.navigate('HomeScreen')
+        })
+    }
+    const isTokenAvailable = () => {
+        recoveryTokenFromStorage().then(function(toke){
+            if (toke == undefined) {
+                return navigation.navigate('LoginScreen')
+            }
+            dispatch({ type: AuthActions.ADD_TOKEN, payload: toke })
+            return navigation.navigate('HomeScreen')
+        })
     }
     return {
         AuthState: state,
         SignIn,
         SignOut,
-        setToken
+        isTokenAvailable
     }
 }
 export default useAuth
--- app/Utils/Storage.js
+++ app/Utils/Storage.js
@@ -1,14 +1,15 @@
 import * as SecureStore from 'expo-secure-store'
 import { Alert } from 'react-native'
-export async function saveToken(key, value) {
-    return await SecureStore.setItemAsync(key, value)
+export async function saveTokenInStorage(token) {
+    await SecureStore.setItemAsync('AuthToken', token)
 }
-export async function obtainToken(key) {
-    let storage = await SecureStore.getItemAsync(key)
+export async function removeTokenFromStorage() {
+    await SecureStore.deleteItemAsync('AuthToken')
+}
+export async function recoveryTokenFromStorage() {
+    const storage = await SecureStore.getItemAsync('AuthToken')
     if (storage) {
         return storage
     }
-}
-export async function removeToken() {
-    await SecureStore.deleteItemAsync('token')
+    return undefined
 }
