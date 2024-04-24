c1c33793af0444b8874b042125f4a9f5ce8b03e0
DanielCArce <daniel.camposarce@gmail.com>
2024-04-24 00:16:18 -0600

    validate if already have orders

--- app/Screens/DashboardTab.jsx
+++ app/Screens/DashboardTab.jsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, SafeAreaView, Platform, ActivityIndicator } from 'react-native'
+import { View, FlatList, SafeAreaView, Platform, ActivityIndicator } from 'react-native'
 import React, { useEffect } from 'react'
 import useContent from '../Hooks/useContent'
 import OrderCard from '../Components/OrderCard'
@@ -7,12 +7,14 @@ import AppHeader from '../Components/AppHeader'
 const DashboardTab = () => {
   const { ContentState, getOrders } = useContent()
   useEffect(function () {
-    getOrders()
+    if (!ContentState.orders.length >= 0) {
+      getOrders()
+    }
   },[])
   return (
       <SafeAreaView>
         <View style={[Platform.OS == "web" || Platform.OS == "android" ? { marginTop: 50, marginHorizontal:30 } : null]}>
-          <FlatList data={ContentState.orders} renderItem={({ item }) => <OrderCard orderInfo={item}/>} keyExtractor={(item) => item.orderID} ListHeaderComponent={<AppHeader />} ListEmptyComponent={<ActivityIndicator/> }  />
+        <FlatList data={ContentState.orders} renderItem={({ item }) => <OrderCard orderInfo={item} />} keyExtractor={(item) => item.orderID} ListHeaderComponent={<AppHeader />} ListEmptyComponent={<ActivityIndicator />} refreshing={true} />
       </View>
     </SafeAreaView>
   )
