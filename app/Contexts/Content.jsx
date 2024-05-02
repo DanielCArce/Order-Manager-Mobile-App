7c5a72f37e238147c4398e61e6cd4ca4540339dd
DanielCArce <daniel.camposarce@gmail.com>
2024-05-02 11:24:25 -0600

    fix content and DashboardTab
    Added filterBy in Contexts

--- app/Contexts/Content.jsx
+++ app/Contexts/Content.jsx
@@ -2,7 +2,7 @@ import { createContext, useReducer } from 'react'
 import ContentReducer from '../Reducers/Content'
 export const ContentContext = createContext(null)
 export default function ContentProvider({ children }) {
-    const [state,dispatch ] = useReducer(ContentReducer,{orders:[], order:{}, clients:[], shippings:[], filterBy:'ALL'})
+    const [state,dispatch ] = useReducer(ContentReducer,{orders:[], order:{}, clients:[], shippings:[], filterBy:'ON_PROCESS'})
     return (
         <ContentContext.Provider value={{ state, dispatch }}>
             {children}
--- app/Screens/DashboardTab.jsx
+++ app/Screens/DashboardTab.jsx
@@ -1,47 +1,21 @@
 import { View, FlatList, SafeAreaView, Platform, ActivityIndicator, Text } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import useContent from '../Hooks/useContent'
 import OrderCard from '../Components/OrderCard'
 import AppHeader from '../Components/AppHeader'
-import FilterOrders from '../Components/FilterOrders'
 
 const DashboardTab = () => {
-  const [orders, setOrders] = useState([])
   const { ContentState, getOrders } = useContent()
   useEffect(function () {
     getOrders()
-    let porers = ContentState.orders.filter((order) => {
-      if (ContentState.filterBy == 'ON_PROCESS') {
-        return order.status == 'ON_PROCESS'
-      }
-      if (ContentState.filterBy == 'COMPLETED') {
-        return order.status == 'COMPLETED'
-      }
-      return order
-
-    })
-    setOrders(porers)
   }, [])
-  useEffect(function () { 
-    let porers = ContentState.orders.filter((order) => {
-      if (ContentState.filterBy == 'ON_PROCESS') {
-        return order.status == 'ON_PROCESS'
-      }
-      if (ContentState.filterBy == 'COMPLETED') {
-        return order.status == 'COMPLETED'
-      }
-      return order
-
-    })
-    setOrders(porers)
-  },[ContentState.filterBy])
   return (
       <SafeAreaView>
       <View style={[Platform.OS == "web" || Platform.OS == "android" ? { marginTop: 50, marginHorizontal: 30 } : null]}>
         {/* <Text>{ JSON.stringify(ContentState.orders)}</Text> */}
-        <FlatList data={orders} renderItem={({ item }) => <OrderCard orderInfo={item} />} keyExtractor={(item) => item.orderID} ListEmptyComponent={<ActivityIndicator/>} refreshing={true} ListHeaderComponent={<>
+        <FlatList data={ContentState.orders} renderItem={({ item }) => <OrderCard orderInfo={item} />} keyExtractor={(item) => item.orderID} ListEmptyComponent={<ActivityIndicator/>} refreshing={true} ListHeaderComponent={<>
         <AppHeader/> 
-        <Text style={{marginVertical:15, fontWeight:'600', fontSize:26, textAlign:'center'}}>Ordenes Pendientes</Text><FilterOrders/></>}/>
+        <Text style={{marginVertical:15, fontWeight:'600', fontSize:26, textAlign:'center'}}>Ordenes Pendientes</Text></>}/>
       </View>
     </SafeAreaView>
   )
