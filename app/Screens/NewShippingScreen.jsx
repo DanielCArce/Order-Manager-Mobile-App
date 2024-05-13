21fd77879a27360601ed276e7c37fac459ba48f7
DanielCArce <daniel.camposarce@gmail.com>
2024-05-13 00:56:31 -0600

    remove formik custom fields
    Remove Form, Field and avoid a transformation
    to input like a web

--- app/Screens/NewShippingScreen.jsx
+++ app/Screens/NewShippingScreen.jsx
@@ -1,52 +1,50 @@
-import { View, Text, Pressable } from 'react-native'
-import React, { useReducer } from 'react'
+import { View, Text, Pressable, TextInput } from 'react-native'
+import React from 'react'
 import { useRoute } from '@react-navigation/native'
-import { Field, FieldArray, Form, Formik } from 'formik'
+import {Formik, FieldArray, Field} from 'formik'
 import useContent from '../Hooks/useContent'
 
 const NewShippingScreen = () => {
     const {params} = useRoute()
     const {addShippingToOrder} = useContent()
   return (
-    <View style={{marginHorizontal:30}}>
-          <Text>NewShippingScreen</Text>
-          <Text>Info: {JSON.stringify(params)}</Text>
-          {console.log(params.oInfo)}
-          <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-              <Text>Descripción</Text>
-              <Text>Cantidad</Text>
-          </View>
-          <Formik initialValues={params.oInfo} onSubmit={(values) => {
-              console.log({ values })
-              addShippingToOrder({orderID: values.orderID,shipping:{items:values.items}})
-          }}>
+    <View style={{marginHorizontal:30, gap:20}}>
+          <Text>Entrega Para Orden # {params.oInfo.orderID }</Text>
+          {/* <Text>Info: {JSON.stringify(params)}</Text>
+          {console.log(params.oInfo)} */}
+          <Formik initialValues={params.oInfo} onSubmit={
+              (values) => {
+                  addShippingToOrder({orderID: values.orderID,shipping:{items:values.items}})
+              }
+          }>
               {
-                  ({ values, handleSubmit }) => {
-                      return (<>
-                          <Form>
+                  ({values, handleChange, handleSubmit, handleBlur }) => {
+                      return (
+                          <>
+                              <View style={{flexDirection:'row', justifyContent:'space-between', borderBottomWidth:2}}>
+                                <Text>Descripción</Text>
+                                <Text>Cantidad</Text>
+                              </View>
                               <FieldArray name='items'>
-                                  {
-                                      ({form }) => {
+                              {({ form }) => {
+                                  return (
+                                      form.values.items.map((item, index) => {
                                           return (
-                                              form.values.items.map((item, index) => {
-                                                  return (
-                                                      <View key={item.id} style={{flexDirection:'row', justifyContent:'space-between'}}>
-                                                          <View>
-                                                              <Text>{`${item.unid.slice(0, 3)} ${item.description} ${item.type.slice(0, 3)} ${item.size}\"`}</Text>
-                                                          </View>
-                                                          <Field style={{flex:0.40}} name={`items[${index}].quantity`} />
-                                                      </View>
-                                                  )
-                                              })
+                                              <View key={item.id} style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom:15, gap:10 }}>
+                                                  <View>
+                                                      <Text>{`${item.unid.slice(0, 3)} ${item.description} ${item.type.slice(0, 3)} ${item.size}\"`}</Text>
+                                                  </View>
+                                                  <TextInput onBlur={handleBlur(`items[${index}].quantity`)} onChangeText={handleChange(`items[${index}].quantity`)} value={`${item.quantity}`} style={{borderWidth:1, flex:.35, textAlign:'center'}} />
+                                              </View>
                                           )
-                                      }
-                                  }
-                              </FieldArray>
-                              <Pressable onPress={ handleSubmit}>
+                                      })
+                                  )
+                              } }
+                          </FieldArray>
+                              <Pressable onPress={handleSubmit} style={{backgroundColor:'#3ea3ae'}}>
                                   <Text>Actualizar</Text>
-                                  </Pressable>
-                          </Form>
-                      </>)
+                              </Pressable></>
+                      )
                   }
               }
           </Formik>
