b50f83be02844428a3b54df0f8224454055b0b90
DanielCArce <daniel.camposarce@gmail.com>
2024-05-18 23:03:24 -0600

    add test signin form
    Add a basic test for SignInForm

--- __test__/Login.test.js
+++ __test__/Login.test.js
@@ -1,11 +1,18 @@
 import { fireEvent, render, screen } from '@testing-library/react-native'
-import LoginScreen from './../app/Screens/LoginScreen.jsx';
-describe('LoginScreen', () => {
-    test('Should be render', () => {
-        render(<LoginScreen />)
-        expect(screen.getAllByTestId('email').value).toBe('')
-    });
-    test('First Test', function () {
-        expect('11').toBe('11')
-    })
-});
+
+import LoginScreen from '../app/Components/SignInForm'
+import AuthProvider from '../app/Contexts/Auth'
+import { NavigationContainer } from '@react-navigation/native'
+
+const ComponentForTest = () => (
+    <AuthProvider>
+        <NavigationContainer>
+        <LoginScreen/>
+        </NavigationContainer>
+    </AuthProvider>
+)
+it('Check if Component Email', function () {
+    render(<ComponentForTest/>)
+    expect(screen.getByTestId('email')).toBeTruthy()
+
+})
