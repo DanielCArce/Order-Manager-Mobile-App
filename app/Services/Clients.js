65ae5abaf387a8b903aeef5a2fbc2d6b29856806
DanielCArce <daniel.camposarce@gmail.com>
2024-05-21 01:12:35 -0600

    add service for update client info

--- app/Services/Clients.js
+++ app/Services/Clients.js
@@ -16,3 +16,44 @@ export async function getAllClients(token) {
         throw new Error(error.message)
     }
 }
+export async function createClient(token, payload) {
+    const abortController = new AbortController()
+    try {
+        const content = JSON.stringify(payload)
+        const request = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/companies/new-company`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: content,
+            signal:abortController.signal
+        })
+        const response = await request.json()
+        const { newCompany } = response
+        return newCompany
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
+export async function updateClientInfo(token, companyID, payload) {
+ const abortController = new AbortController()
+    try {
+        const content = JSON.stringify(payload)
+        console.log({content, token, companyID, payload})
+        const request = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/companies/${companyID}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: content,
+            signal:abortController.signal
+        })
+        const response = await request.json()
+        const { companyUpdated } = response
+        return companyUpdated
+    } catch (error) {
+        throw new Error(error.message)
+    }   
+}
