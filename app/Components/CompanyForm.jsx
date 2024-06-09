8f074ae082ab5bf21046a49b0d3981ae9533c7a6
DanielCArce <daniel.camposarce@gmail.com>
2024-06-09 04:00:26 -0600

    extract new company from outside screen

---
+++ app/Components/CompanyForm.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {Formik} from 'formik'
+const CompanyForm = () => {
+  return (
+    <View>
+      <Text>CompanyForm</Text>
+    </View>
+  )
+}
+
+export default CompanyForm
---
+++ app/Screens/AddNewCompanyScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import CompanyForm from '../Components/CompanyForm'
+
+const AddNewCompanyScreen = () => {
+  return (
+    <View>
+      <Text>AddNewCompanyScreen</Text>
+      <CompanyForm/>
+    </View>
+  )
+}
+
+export default AddNewCompanyScreen
