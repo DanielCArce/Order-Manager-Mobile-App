49acaa2a6e49dc236f9bc61a026ef944dec66570
DanielCArce <daniel.camposarce@gmail.com>
2024-06-11 13:02:17 -0600

    redesign modal

--- app/Components/Modal.jsx
+++ app/Components/Modal.jsx
@@ -4,7 +4,7 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import useContent from '../Hooks/useContent';
 const Modal = ({ isShow, info, click }) => {
     const [data, setData] = useState({})
-    const {updateClient} = useContent()
+    const {updateClient,ContentState} = useContent()
     const toggleInfo = (val) => {
         setData((prevVal) => {
             return {
@@ -15,43 +15,27 @@ const Modal = ({ isShow, info, click }) => {
         updateClient(data.id, {"isFE": data.isFE})
     }
 useEffect(function () {
-        setData(info)
+    setData(info)
+    console.log({info})
 }, [info])
     
   return (
-      <ModalRN animationType='slide' visible={isShow} >
-          
-          <View key={data.id} style={{paddingVertical:15, paddingHorizontal:30}}>
-              <Pressable onPress={click} style={{alignSelf:'center'}}>
-                  <MaterialCommunityIcons size={24 } name='close'/>
-              </Pressable>
-          <View style={styles.rows}>
-              <View>
-                  <Text>Nombre:</Text>
-              </View>
-              <View>
-                  <Text>{ data.name}</Text>
-              </View>
-          </View>
-          <View style={styles.rows}>
-              <View>
+      <ModalRN animationType='slide' visible={isShow} transparent={true}>
+          <View style={{ backgroundColor: 'lightgray', flex: 1, justifyContent: 'center', alignItems: 'center', paddingVertical: 35, paddingHorizontal: 10, borderRadius: 15}}>
+              <View style={{ backgroundColor: 'white', flex: .85, paddingVertical: 15, paddingHorizontal: 10, gap:15, borderRadius:5 }}>
+                  <MaterialCommunityIcons size={24} name="close" style={{alignSelf:'center'} } onPress={click}/>
+                  <Text style={{ fontWeight: '700', fontSize: 26, fontStyle: 'italic', textTransform: 'uppercase', textAlign: 'center' }}>{data.name}</Text>
+                  <Text style={{ fontWeight: '400', fontSize: 16, textTransform: 'capitalize', textAlign: 'center' }}>{data.type == "FACTORY" ? "Fabricante" : "Peletero"}</Text>
+                  <Text>Correo Electrónico: {data.email}</Text>
                   <Text>Factura Electrónica:</Text>
-              </View>
-              <View>
-                      <Switch value={data.isFE} trackColor={{ false: 'red', true: 'blue' }} thumbColor={info.isFE ? 'orange' : 'gray'} onValueChange={toggleInfo} />
-              </View>
-          </View>
-          <View style={styles.rows}>
-              <View>
-                  <Text>Correo Electrónico:</Text>
-              </View>
-              <View>
-                  <Text>
-                      {data.email}
-                  </Text>
+                  <View style={{flexDirection:'row', gap:5, alignItems:'center'}}>
+                      <Text>Si</Text>
+                      <Switch onValueChange={toggleInfo} value={data.isFE} thumbColor={data.isFE ? '#0080ff' : '#c0c0c0' } trackColor={{false:'#c0c0c0', true:'#0080ff'}}/>
+                      <Text>No</Text>
+                    </View>
+                  <Text>Tiene {ContentState.orders.filter((val)=> val.clientID == data.id).length } ordenes realizadas.</Text>
               </View>
           </View>
-      </View>
           </ModalRN>
   )
 }
