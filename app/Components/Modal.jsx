29efd4ec295db4c39e1e25d2d4c6e0cba0aea919
DanielCArce <daniel.camposarce@gmail.com>
2024-07-01 17:54:11 -0600

    feat: add update status in modal
    Add modal switch electronic invoice flag
    to update directly to db.

--- app/Components/Modal.jsx
+++ app/Components/Modal.jsx
@@ -2,11 +2,10 @@ import { View, Text, Modal as ModalRN, Switch, StyleSheet } from 'react-native';
 import React from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import useContent from '../Hooks/useContent';
-const Modal = ({ isShow, info, click }) => {
+const Modal = ({ isShow, click }) => {
     const {updateClient,ContentState} = useContent()
     const toggleInfo = (val) => {
-        console.log({info})
-        updateClient(info.id, {"isFE": info.isFE})
+        updateClient(ContentState.client.id, { "isFE": val })
     }
     
   return (
@@ -14,16 +13,17 @@ const Modal = ({ isShow, info, click }) => {
           <View style={{ backgroundColor: 'lightgray', flex: 1, justifyContent: 'center', alignItems: 'center', paddingVertical: 35, paddingHorizontal: 10, borderRadius: 15}}>
               <View style={{ backgroundColor: 'white', flex: .85, paddingVertical: 15, paddingHorizontal: 10, gap:15, borderRadius:5 }}>
                   <MaterialCommunityIcons size={24} name="close" style={{alignSelf:'center'} } onPress={click}/>
-                  <Text style={{ fontWeight: '700', fontSize: 26, fontStyle: 'italic', textTransform: 'uppercase', textAlign: 'center' }}>{info.name}</Text>
-                  <Text style={{ fontWeight: '400', fontSize: 16, textTransform: 'capitalize', textAlign: 'center' }}>{info.type == "FACTORY" ? "Fabricante" : "Peletero"}</Text>
-                  <Text>Correo Electrónico: {info.email}</Text>
+                  <Text style={{ fontWeight: '700', fontSize: 26, fontStyle: 'italic', textTransform: 'uppercase', textAlign: 'center' }}>{ContentState.client.name}</Text>
+                  <Text style={{ fontWeight: '400', fontSize: 16, textTransform: 'capitalize', textAlign: 'center' }}>{ContentState.client.type == "FACTORY" ? "Fabricante" : "Peletero"}</Text>
+                  <Text>Correo Electrónico: {ContentState.client.email}</Text>
                   <Text>Factura Electrónica:</Text>
-                  <View style={{flexDirection:'row', gap:5, alignItems:'center'}}>
-                      <Text>Si</Text>
-                      <Switch onValueChange={toggleInfo} value={info.isFE} thumbColor={info.isFE ? '#0080ff' : '#c0c0c0' } trackColor={{false:'#c0c0c0', true:'#0080ff'}}/>
+                  <View style={{ flexDirection: 'row', gap: 5, alignItems: 'center' }}>
+                      <Text>{ JSON.stringify(ContentState.client.isFE)}</Text>
                       <Text>No</Text>
+                      <Switch onValueChange={toggleInfo} value={ContentState.client.isFE} thumbColor={ContentState.client.isFE ? '#0080ff' : '#c0c0c0' } trackColor={{false:'#c0c0c0', true:'#0080ff'}}/>
+                      <Text>Si</Text>
                     </View>
-                  <Text>Tiene {ContentState.orders.filter((val)=> val.clientID == info.id).length } ordenes realizadas.</Text>
+                  <Text>Tiene {ContentState.orders.filter((val)=> val.clientID == ContentState.client.id).length } ordenes realizadas.</Text>
               </View>
           </View>
           </ModalRN>
